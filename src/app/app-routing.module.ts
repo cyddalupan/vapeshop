2fea8f76e3ec06d4a994991d837d3b44d9f88ab6
Cyd Dalupan <cydmdalupan@gmail.com>
2023-07-20 11:39:58 +0000

    Create MVP pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { PosComponent } from './pages/pos/pos.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'pos', component: PosComponent },
+  { path: 'reports', component: ReportsComponent },
+
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
+	{ path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { PosComponent } from './pages/pos/pos.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InventoryComponent,
+    PosComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,

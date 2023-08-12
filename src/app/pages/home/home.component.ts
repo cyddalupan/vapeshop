26c69f4cb4265e4a97a4a60c9700ebbda39bfeac
Cleddrence Dalupan <cydmdalupan@gmail.com>
2023-08-12 14:56:48 +0800

    Add Upload button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,27 +14,27 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.store.dispatch(AddItem({
-      id: 111,
-      code: 4800488959878,
-      name: 'Nursy Wet Wipes',
-      price: 133,
-			desc: "random desc",
-      created_at: '2012',
-      updated_at: '2013',
-      deleted_at: null
-    }));
+    // this.store.dispatch(AddItem({
+    //   id: 111,
+    //   code: 4800488959878,
+    //   name: 'Nursy Wet Wipes',
+    //   price: 133,
+		// 	desc: "random desc",
+    //   created_at: '2012',
+    //   updated_at: '2013',
+    //   deleted_at: null
+    // }));
 
-    this.store.dispatch(AddItem({
-      id: 222,
-      code: 16000439894,
-      name: 'Granola Bar',
-      price: 321,
-			desc: "another desc",
-      created_at: '2012',
-      updated_at: '2013',
-      deleted_at: null
-    }));
+    // this.store.dispatch(AddItem({
+    //   id: 222,
+    //   code: 16000439894,
+    //   name: 'Granola Bar',
+    //   price: 321,
+		// 	desc: "another desc",
+    //   created_at: '2012',
+    //   updated_at: '2013',
+    //   deleted_at: null
+    // }));
   }
   
 }

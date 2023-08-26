d2b546f81cd4bae844e53e93f44e0e145d4e1dc3
Cleddrence Dalupan <cydmdalupan@gmail.com>
2023-08-26 17:02:37 +0800

    Design reports page

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reports',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent {
+  formattedDate = new Date().toISOString().substr(0, 10);
 
+  public reportForm: FormGroup = new FormGroup({
+		startdate: new FormControl(this.formattedDate, [Validators.required]),
+		enddate: new FormControl(this.formattedDate, [Validators.required]),
+  });
+
+  onSubmit() {
+    console.log("submit trigger");
+  }
 }
